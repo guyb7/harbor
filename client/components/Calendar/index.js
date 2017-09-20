1cad807540cb8e4e34ad450ed37d9c356a699bd7
Guy Brukhis <guy.br7@gmail.com>
2017-09-20 16:42:46 +0300

    render agenda calendar days

--- client/components/Calendar/index.js
+++ client/components/Calendar/index.js
@@ -1,36 +1,55 @@
 import React, { Component } from 'react'
+import moment from 'moment'
 import _ from 'lodash'
 
 import { Grid } from 'semantic-ui-react'
 import style from './style'
 
 class Calendar extends Component {
-  getWeek(weekNum) {
-    const days = []
-    _.times(7, n => {
-      days.push(this.getDay(n + 1 + (weekNum * 7)))
-    })
+  getWeekRow(days, key) {
     return (
-      <Grid.Row columns={7} key={weekNum}>
+      <Grid.Row columns={7} key={key}>
         {days}
       </Grid.Row>
     )
   }
 
-  getDay(monthDay) {
+  getDayCell(monthDay, key) {
     return (
-      <Grid.Column style={style.day} key={monthDay}>
+      <Grid.Column style={style.day} key={monthDay || key}>
         {monthDay}
       </Grid.Column>
     )
   }
 
-  render() {
+  getMonthWeeks() {
+    let week = []
     const weeks = []
-    _.times(5, n => {
-      weeks.push(this.getWeek(n))
+    const monthDate = moment(`${this.props.year}-${this.props.month}-01`, 'YYYY-MM-DD')
+    // Add empty cells to first week
+    _.times(monthDate.day(), n => {
+       week.push(this.getDayCell(null, 'k1' + n))
     })
+    _.times(monthDate.daysInMonth(), n => {
+      if (monthDate.day() === 0 && n > 0) {
+        weeks.push(this.getWeekRow(week, weeks.length))
+        week = []
+      }
+       week.push(this.getDayCell(monthDate.date()))
+       monthDate.add(1, 'day')
+    })
+    // Add empty cells to last week
+    if (monthDate.day() > 0) {
+      _.times(7 - monthDate.day(), n => {
+         week.push(this.getDayCell(null, 'k2' + n))
+      })
+    }
+    weeks.push(this.getWeekRow(week, weeks.length))
+    return weeks
+  }
 
+  render() {
+    const weeks = this.getMonthWeeks()
     return (
       <Grid celled>
         {weeks}
--- client/components/views/Agenda/index.js
+++ client/components/views/Agenda/index.js
@@ -14,16 +14,16 @@ class Agenda extends Component {
     return (
       <div style={style.container}>
         <h4>January</h4>
-        <Calendar events={[]} />
+        <Calendar year="2018" month="01" events={[]} />
 
         <h4>February</h4>
-        <Calendar events={[]} />
+        <Calendar year="2018" month="02" events={[]} />
 
         <h4>March</h4>
-        <Calendar events={[]} />
+        <Calendar year="2018" month="03" events={[]} />
 
         <h4>April</h4>
-        <Calendar events={[]} />
+        <Calendar year="2018" month="04" events={[]} />
       </div>
     )
   }
