96c155c8a5f0a3b61f5da7e402e8cadd016eaad1
Guy Brukhis <guy.br7@gmail.com>
2017-10-07 02:09:45 +0300

    mentor page improvements

--- client/components/views/Person/index.js
+++ client/components/views/Person/index.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import * as actions from '../../../store/action-creators'
 
-import { Divider, Image, Item, Label, List } from 'semantic-ui-react'
+import { Button, Divider, Image, Item, Label, List } from 'semantic-ui-react'
 import PersonListItem from '../../Person/ListItem'
 import StartupItem from '../../Startup/Item'
-import startupLogo2 from '../../../assets/placeholder.png'
-// import startupLogo2 from '../../../assets/startup-reesio.png'
+import startupLogo1 from '../../../assets/startup-canva.png'
+import startupLogo2 from '../../../assets/startup-reesio.png'
+import startupLogo3 from '../../../assets/startup-tuna.jpg'
 import userAvatar4 from '../../../assets/user4.jpg'
 import userAvatar5 from '../../../assets/user5.jpg'
 import style from './style'
@@ -28,13 +29,13 @@ My job is to design and build modern systems so that everything is functional an
       { icon: 'linkedin', content: 'Otis Cruickshank' },
       { content: 'Website' }
     ],
-    startup: {
+    startups: [{
       id: 'reesio',
       name: 'Reesio',
       logo: startupLogo2,
       description: 'Online real estate',
       labels: ['Real Estate']
-    }
+    }]
   },
   'gerry-kunde': {
     id: 'gerry-kunde',
@@ -43,7 +44,34 @@ My job is to design and build modern systems so that everything is functional an
     avatar: userAvatar4,
     title: 'Investor',
     labels: ['Angel'],
-    description: 'Reesio, CTO, Reesio, CTO, Reesio, CTO, Reesio, CTO',
+    description: `Gerry has over 25-years of experience investing in the transformation of energy markets and the development of lower carbon energy solutions, both as a principal investor and as a corporate and project financier.
+
+He ran the investment business of Climate Change Capital (CCC) in China until 2010, funding the development of clean energy and emission reduction projects through the adoption of innovative technology solutions. He managed an investment portfolio of over €500m.`,
+    links: [
+      { icon: 'mail', content: 'gerry@kundleinvestments.com', link: 'mailto:gerry@kundleinvestments.com' },
+      { icon: 'linkedin', content: 'Gerry Kunde' },
+    ],
+    startups: [
+      {
+        id: 'canva',
+        name: 'Canva',
+        logo: startupLogo1,
+        description: 'Graphic design tool',
+        labels: ['Design', 'Tools']
+      }, {
+        id: 'reesio',
+        name: 'Reesio',
+        logo: startupLogo2,
+        description: 'Online real estate',
+        labels: ['Real Estate']
+      }, {
+        id: 'tuna',
+        name: 'Tuna',
+        logo: startupLogo3,
+        description: 'Musicians Collaboration',
+        labels: ['Music']
+      }
+    ]
   }
 }
 
@@ -59,7 +87,6 @@ class Person extends Component {
       labels: [],
       description: null,
       links: [],
-      startup: null,
       startups: []
     }
   }
@@ -92,6 +119,32 @@ class Person extends Component {
                 this.state.type === 'accelerator-member' && <Label color='red' attached='bottom left'>Accelerator Member</Label>
               }
             </div>
+            <Button.Group basic fluid vertical>
+              {
+                this.props.user.type === 'startup-member' &&
+                <Button content='Add Mentor' icon='pin' labelPosition='left' />
+              }
+              <Button content='Message' icon='mail outline' labelPosition='left' />
+            </Button.Group>
+            {
+              this.state.startups && this.state.startups.length > 0 && <div>
+                <Divider section />
+                {
+                  this.state.startups.map(s => <StartupItem startup={s} key={s.id} />)
+                }
+              </div>
+            }
+          </div>
+          <div style={style.profile}>
+            <Item.Group>
+              <Item>
+                <Item.Content>
+                  <Item.Header style={style.name}>{this.state.name}</Item.Header>
+                  <Item.Meta>{this.state.title}</Item.Meta>
+                </Item.Content>
+              </Item>
+              <Item style={style.description} description={this.state.description} />
+            </Item.Group>
             <div>
               {
                 this.state.labels.map(l => <Label horizontal key={l}>{l}</Label>)
@@ -106,23 +159,6 @@ class Person extends Component {
                 />)
               }
             </List>
-            {
-              this.state.startup && <div>
-                <Divider section />
-                <StartupItem startup={this.state.startup} />
-              </div>
-            }
-          </div>
-          <div style={style.profile}>
-            <Item.Group>
-              <Item>
-                <Item.Content>
-                  <Item.Header style={style.name}>{this.state.name}</Item.Header>
-                  <Item.Meta>{this.state.title}</Item.Meta>
-                </Item.Content>
-              </Item>
-              <Item style={style.description} description={this.state.description} />
-            </Item.Group>
           </div>
         </div>
       </div>
@@ -130,4 +166,6 @@ class Person extends Component {
   }
 }
 
-export default connect()(Person)
+export default connect(state => ({
+  user: state.user
+}))(Person)
